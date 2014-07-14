806809df52626cc1e34db5d2c8524e4f80dfa3df
Sarah Kate Mann <sarah@Sarahs-MacBook-Pro.local>
2014-07-14 20:15:03 +1000

    Added a test class

---
+++ PGTest.h
@@ -0,0 +1,13 @@
+//
+//  PGTest.h
+//  GitTest
+//
+//  Created by Sarah Kate Mann on 14/07/2014.
+//  Copyright (c) 2014 Sarah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PGTest : NSObject
+
+@end
---
+++ PGTest.m
@@ -0,0 +1,13 @@
+//
+//  PGTest.m
+//  GitTest
+//
+//  Created by Sarah Kate Mann on 14/07/2014.
+//  Copyright (c) 2014 Sarah. All rights reserved.
+//
+
+#import "PGTest.h"
+
+@implementation PGTest
+
+@end
